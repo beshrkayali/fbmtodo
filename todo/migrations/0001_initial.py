bbfb1a5c2f65f727b4977bb0ef0c8d457098818d
Beshr Kayali <beshrkayali@gmail.com>
2015-04-15 01:18:30 +0200

    Removed unnecessary migrations / updated models...

--- todo/migrations/0001_initial.py
+++ todo/migrations/0001_initial.py
@@ -8,18 +8,34 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
+            name='User',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('password', models.CharField(max_length=128, verbose_name='password')),
+                ('last_login', models.DateTimeField(null=True, verbose_name='last login', blank=True)),
+                ('email', models.EmailField(unique=True, max_length=255)),
+                ('joined', models.DateTimeField(auto_now_add=True)),
+                ('is_active', models.BooleanField(default=True)),
+                ('is_admin', models.BooleanField(default=False)),
+            ],
+            options={
+                'abstract': False,
+            },
+        ),
+        migrations.CreateModel(
             name='TodoItem',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('done', models.BooleanField(default=False)),
                 ('text', models.TextField()),
-                ('priority', models.IntegerField(default=1, choices=[(1, b'Low'), (2, b'Medium'), (3, b'High'), (4, b'Super high')])),
+                ('priority', models.IntegerField(default=1, choices=[(1, b'Low'), (2, b'Medium'), (3, b'High')])),
                 ('timestamp', models.DateTimeField(auto_now_add=True)),
                 ('lastedit', models.DateTimeField(auto_now=True)),
+                ('owner', models.ForeignKey(related_name='todos', to=settings.AUTH_USER_MODEL)),
             ],
         ),
         migrations.CreateModel(
@@ -27,7 +43,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=256)),
-                ('desc', models.TextField()),
+                ('desc', models.TextField(null=True, blank=True)),
                 ('owner', models.ForeignKey(related_name='todolists', to=settings.AUTH_USER_MODEL)),
             ],
         ),
@@ -35,10 +51,5 @@ class Migration(migrations.Migration):
             model_name='todoitem',
             name='todolist',
             field=models.ForeignKey(related_name='todos', to='todo.TodoList'),
-        ),
-        migrations.AddField(
-            model_name='todoitem',
-            name='user',
-            field=models.ForeignKey(related_name='todos', to=settings.AUTH_USER_MODEL),
         ),
     ]
--- todo/models.py
+++ todo/models.py
@@ -1,5 +1,67 @@
 from django.db import models
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
+from django.contrib.auth.models import (BaseUserManager,
+                                        AbstractBaseUser)
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(email=self.normalize_email(email))
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(email, password=password)
+
+        user.is_admin = True
+
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=255, unique=True)
+    joined = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+
+    objects = UserManager()
+
+    def get_full_name(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.email.split("@")[0]
+
+    @property
+    def is_superuser(self):
+        return self.is_admin
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return self.is_admin
+
+    def __unicode__(self):
+        return self.email
 
 
 class TodoList(models.Model):

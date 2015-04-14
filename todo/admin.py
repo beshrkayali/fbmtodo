694b08dec5507fe70fbe25cb093d93c12a836baa
Beshr Kayali <beshrkayali@gmail.com>
2015-04-15 01:19:01 +0200

    Show custom user in admin

--- todo/admin.py
+++ todo/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from todo.models import TodoList, TodoItem
+from todo.models import TodoList, TodoItem, User
 
+admin.site.register(User)
 admin.site.register(TodoList)
 admin.site.register(TodoItem)

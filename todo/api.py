d5dd5dab6575beb358d36633d58cdadb4aed5e54
Beshr Kayali <beshrkayali@gmail.com>
2015-04-14 07:38:23 +0200

    Basic API Viewsets/Serializers

---
+++ todo/api.py
@@ -0,0 +1,72 @@
+from .models import TodoList, TodoItem
+
+from rest_framework import serializers, viewsets
+from rest_framework import permissions
+from rest_framework.exceptions import PermissionDenied
+
+
+# Generic permission to get user's own objects
+class OwnerOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.owner == request.user
+
+
+class BaseTodoViewset(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated, OwnerOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+# Todo Item Serializer / Viewset
+class TodoItemSerializer(serializers.ModelSerializer):
+
+    def __init__(self, *args, **kwargs):
+        super(TodoItemSerializer, self).__init__(*args, **kwargs)
+
+        if hasattr(self, 'user'):
+            self.fields['todolist'] = serializers.PrimaryKeyRelatedField(
+                queryset=TodoList.objects.filter(owner=self.user)
+            )
+
+    class Meta:
+        model = TodoItem
+        fields = ('id', 'todolist', 'done', 'text',
+                  'priority', 'timestamp', 'lastedit')
+
+
+class TodoItemViewset(BaseTodoViewset):
+
+    def get_serializer_class(self):
+        class Serializer(TodoItemSerializer):
+            user = self.request.user
+
+        return Serializer
+
+    def perform_create(self, serializer):
+        # Make sure todo being created is owned by authenticated user
+        todolist_pk = serializer.data.get('todolist')
+        if todolist_pk and \
+           TodoList.objects.get(pk=todolist_pk).owner == self.request.user:
+            super(TodoItemViewset, self).perform_create(serializer)
+        else:
+            raise PermissionDenied()
+
+    def get_queryset(self):
+        return self.request.user.todos.all()
+
+
+# Todo List Serializer / Viewset
+class TodoListSerializer(serializers.ModelSerializer):
+    todos = TodoItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = TodoList
+        fields = ('id', 'name', 'desc', 'todos')
+
+
+class TodoListViewset(BaseTodoViewset):
+    serializer_class = TodoListSerializer
+
+    def get_queryset(self):
+        return self.request.user.todolists.all()

dd7a5f9c50ce1d6f6c2da2f454efa05b8d5fd74e
Beshr Kayali <beshrkayali@gmail.com>
2015-04-14 07:38:54 +0200

    Added API routers to urls.py

--- fbmtodo/urls.py
+++ fbmtodo/urls.py
@@ -1,17 +1,34 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = [
-    # Examples:
-    # url(r'^$', 'fbmtodo.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+from todo.api import (TodoListViewset, TodoItemViewset)
+from rest_framework import routers
 
+
+# Register API namespaces
+router = routers.DefaultRouter()
+
+router.register(prefix=r'todolists',
+                viewset=TodoListViewset,
+                base_name='todolists')
+
+router.register(prefix=r'todos',
+                viewset=TodoItemViewset,
+                base_name='todos')
+
+
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 
-    # API stuff
+    # API
+    url(r'^api/', include(router.urls)),
+
+    # API Auth
     url(r'^api-auth/',
         include('rest_framework.urls',
                 namespace='rest_framework')),
 
-    url(r'^$', 'todo.views.home', name='home')
+
+    # Main page
+    url(r'^$', 'todo.views.home', name='home'),
 ]
